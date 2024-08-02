a2b915794c222561e59147b169909658a12614a4
KimJinYeongZ <cheche903@naver.com>
2024-08-02 15:25:35 +0900

    <Feat> : Member 도메인 구현

---
+++ src/main/java/com/example/demo/domain/Member.java
@@ -0,0 +1,44 @@
+package com.example.demo.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Member {
+    private Long memberId;
+    private String username;
+    private String email;
+    private String password;
+
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
