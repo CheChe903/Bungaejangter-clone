63cab290840ee65d22c6dd446c20b6981d550786
KimJinYeongZ <cheche903@naver.com>
2024-08-06 14:40:57 +0900

    <refactor> : getMemberByEmail 메서드 성능 향상
    emailTopIdMap을 따로 하나 더 Map으로 구현.
    중간 매핑을 이용하여 성능 향상 기대

--- src/main/java/com/example/demo/repository/MemberRepository.java
+++ src/main/java/com/example/demo/repository/MemberRepository.java
@@ -7,22 +7,22 @@ import java.util.Map;
 
 public class MemberRepository {
     private final Map<Long, Member> memberMap = new HashMap<>();
+    private final Map<String, Long> emailToIdMap = new HashMap<>();
+
     private long nextId = 1;
 
     public void addMember(Member member) {
         member.setMemberId(nextId++);
         memberMap.put(member.getMemberId(), member);
+        emailToIdMap.put(member.getEmail(), member.getMemberId());
+
     }
     public Member getMemberById(Long memberId) {
         return memberMap.get(memberId);
     }
 
     public Member getMemberByEmail(String email) {
-        for (Member member : memberMap.values()) {
-            if (member.getEmail().equals(email)) {
-                return member;
-            }
-        }
-        return null;
+        Long memberId = emailToIdMap.get(email);
+        return memberId != null ? memberMap.get(memberId) : null;
     }
 }
