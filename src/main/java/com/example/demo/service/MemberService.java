9f9bd9f520d7848a11454a448d09cf3473e17d33
KimJinYeongZ <cheche903@naver.com>
2024-08-12 17:05:05 +0900

    <refactor> : MemberService 리팩토링
    Controller의 역할 분담에 의한 Service 계층에서 메서드 추가 및 활용

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -1,34 +1,55 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Member;
+import com.example.demo.domain.dto.request.MemberLoginRequest;
+import com.example.demo.domain.dto.request.MemberRegisterRequest;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.util.PasswordUtil;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemberRepository();
-    private final PasswordUtil passwordUtil = new PasswordUtil();
+    private final MemberRepository memberRepository;
+    private final PasswordUtil passwordUtil;
 
-    // 회원 가입
-    public void registerMember(String username, String email, String password) {
+    public MemberService(MemberRepository memberRepository, PasswordUtil passwordUtil) {
+        this.memberRepository = memberRepository;
+        this.passwordUtil = passwordUtil;
+    }
+
+    public boolean registerMember(MemberRegisterRequest request) {
+        if (isInvalidRegisterRequest(request)) {
+            return false;
+        }
+
         Member member = new Member();
-        member.setUsername(username);
-        member.setEmail(email);
-        String hashedPassword = passwordUtil.hashPassword(password);
+        member.setUsername(request.getUsername());
+        member.setEmail(request.getEmail());
+        String hashedPassword = passwordUtil.hashPassword(request.getPassword());
         member.setPassword(hashedPassword);
 
         memberRepository.addMember(member);
+        return true;
     }
 
-    // 로그인
-    public Member login(String email, String password) {
-        Member member = memberRepository.getMemberByEmail(email);
-        if (member != null) {
+    public boolean login(MemberLoginRequest request) {
+        if (isInvalidLoginRequest(request)) {
+            return false;
+        }
 
-            String hashedPassword = passwordUtil.hashPassword(password);
+        Member member = memberRepository.getMemberByEmail(request.getEmail());
+        if (member != null) {
+            String hashedPassword = passwordUtil.hashPassword(request.getPassword());
             if (member.getPassword().equals(hashedPassword)) {
-                return member;
+                return true;
             }
         }
-        return null;
+        return false;
+    }
+
+    private boolean isInvalidRegisterRequest(MemberRegisterRequest request) {
+        return request.getUsername() == null || request.getEmail() == null || request.getPassword() == null;
+    }
+
+    private boolean isInvalidLoginRequest(MemberLoginRequest request) {
+        return request.getEmail() == null || request.getPassword() == null;
     }
 }
