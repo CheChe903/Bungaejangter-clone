e24d73d0cc5cd6a7f26643813fd4178a50bc1254
KimJinYeongZ <cheche903@naver.com>
2024-08-15 17:22:44 +0900

    <feat> : ProductService 추가 및 추가 서비스 로직 기능 구현

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+import com.example.demo.domain.Product;
+import com.example.demo.domain.dto.request.Product.AddProductRequest;
+import com.example.demo.domain.dto.response.Member.RegisterResponse;
+import com.example.demo.domain.dto.response.Product.AddProductResponse;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.ProductRepository;
+import com.example.demo.util.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final MemberRepository memberRepository;
+
+    private final JwtUtil jwtUtil;
+
+    public ProductService(ProductRepository productRepository, MemberRepository memberRepository, JwtUtil jwtUtil) {
+        this.productRepository = productRepository;
+        this.memberRepository = memberRepository;
+        this.jwtUtil =jwtUtil;
+    }
+
+
+    public AddProductResponse addProduct(AddProductRequest req, String token) {
+        Member member = memberRepository.getMemberById(jwtUtil.parseToken(token));
+        Product product = new Product(req.getProductName(), req.getDescription(),req.getPrice(),req.getStatus(),req.getImageUrl());
+        product.setMember(member);
+        productRepository.addProduct(product);
+        return new AddProductResponse("Add Product successful", HttpServletResponse.SC_CREATED, true);
+    }
+}
