d0a6d3c169e2fd7a5851ec4abf10a03b977a735a
KimJinYeongZ <cheche903@naver.com>
2024-08-19 15:18:14 +0900

    <feat> : Product Repository에 all 메서드 추가
    모든 상품 반환을 위한 맵을 list로 변환하는 메서드 추가

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -2,8 +2,11 @@ package com.example.demo.repository;
 
 
 import com.example.demo.domain.Product;
+import com.example.demo.domain.dto.ProductDTO;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ProductRepository {
@@ -15,5 +18,24 @@ public class ProductRepository {
     public void addProduct(Product product) {
         product.setProductId(nextId++);
         productMap.put(product.getProductId(), product);
+    }
+
+    public List<ProductDTO> getAllProductList() {
+        List<ProductDTO> products = new ArrayList<>();
+        for(Map.Entry<Long, Product> entry :productMap.entrySet()) {
+            Product product = entry.getValue();
+
+            ProductDTO productDTO = new ProductDTO(
+                    product.getProductId(),
+                    product.getProductName(),
+                    product.getDescription(),
+                    product.getPrice(),
+                    product.getStatus(),
+                    product.getImageUrl()
+            );
+
+            products.add(productDTO);
+        }
+        return products;
     }
 }
