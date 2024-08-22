7c2fc6ce2d3164e84ac71c585816a857cccfbc15
KimJinYeongZ <cheche903@naver.com>
2024-08-22 15:17:19 +0900

    <feat> : Member 정보 반환 서비스 로직 추가
    프로필을 위한 멤버 정보 반환

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -1,14 +1,19 @@
 package com.example.demo.service;
 
 import com.example.demo.domain.Member;
+import com.example.demo.domain.Product;
 import com.example.demo.domain.dto.request.Member.MemberLoginRequest;
 import com.example.demo.domain.dto.request.Member.MemberRegisterRequest;
+import com.example.demo.domain.dto.response.Member.MemberDTO;
+import com.example.demo.domain.dto.response.Product.ProductDTO;
 import com.example.demo.repository.MemberRepository;
 import com.example.demo.util.JwtUtil;
 import com.example.demo.util.PasswordUtil;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class MemberService {
     private final MemberRepository memberRepository;
@@ -47,5 +52,14 @@ public class MemberService {
         } else {
             return null;
         }
+    }
+
+    public MemberDTO getMemberById(Long memberId) {
+        Member member = memberRepository.getMemberById(memberId);
+        List<Product> products = member.getProducts();
+        List<ProductDTO> productDTOs = products.stream()
+                .map(product -> new ProductDTO(product.getProductId(), product.getProductName(), product.getDescription(), product.getPrice(), product.getStatus(), product.getImageUrl()))
+                .collect(Collectors.toList());
+        return new MemberDTO(member.getMemberId(), member.getUsername(), member.getEmail(), productDTOs);
     }
 }
